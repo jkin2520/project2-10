569e92141e97383c42dd8be49aef5a812bcf971a
Jalen King <jkin2520@students.vsu.edu>
2024-12-09 08:38:09 -0500

    Added Javadoc to HashUtils

--- src/src/HashUtils.java
+++ src/src/HashUtils.java
@@ -1,7 +1,25 @@
 package src;
-import java.security.MessageDigest;
 
+import java.security.MessageDigest;
+/**
+ * Utility class for hashing and verifying strings.
+ * 
+ * This class provides methods to:
+ * <ul>
+ * <li>Hash a string using a specified hashing algorithm.</li>
+ * <li>Verify if a string matches a given hash.</li>
+ * <li>Generate a salted hash of a string.</li>
+ * </ul>
+ */
 public class HashUtils {
+    /**
+     * Hashes a string using the specified algorithm.
+     * 
+     * @param input The string to be hashed.
+     * @param algorithm The hashing algorithm to use (MD5, SHA1, or SHA256).
+     * @return The hashed string in hexadecimal format.
+     * @throws RuntimeException If the hashing algorithm is not supported.
+     */
     public static String hash(String input, HashAlgorithm algorithm) {
         try {
             MessageDigest md = MessageDigest.getInstance(algorithm.name().replace("SHA", "SHA-"));
@@ -17,9 +35,26 @@ public class HashUtils {
             throw new RuntimeException("Error hashing string", e);
         }
     }
+    /**
+     * Verifies if a given string matches a given hash using the specified algorithm.
+     * 
+     * @param input The string to verify.
+     * @param hash The expected hash value.
+     * @param algorithm The hashing algorithm used to generate the hash.
+     * @return true if the input matches the hash; false otherwise.
+     */
+
     public static boolean matches(String input, String hash, HashAlgorithm algorithm) {
         return hash.equals(hash(input, algorithm));
     }
+    /**
+     * Generates a salted hash for a given string and salt using the specified algorithm.
+     * 
+     * @param input The string to be hashed.
+     * @param salt The salt to add to the input.
+     * @param algorithm The hashing algorithm to use (MD5, SHA1, or SHA256).
+     * @return The salted hash in hexadecimal format.
+     */
     public static String saltedHash(String input, String salt, HashAlgorithm algorithm) {
         return hash(input + salt, algorithm);
     }
